e8de8a151319b7ded9ca7b3e6c9b20d47d744a57
MARC NAVARRO <navamarc@gmail.com>
2026-04-04 13:34:23 +0200

    chore: update app/src/main/java/com/miseservice/smsovh/data/repository/AdminRepositoryImpl.kt

--- app/src/main/java/com/miseservice/smsovh/data/repository/AdminRepositoryImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.miseservice.smsovh.data.repository
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class AdminRepositoryImpl @Inject constructor()  {
-    // Fichier obsolète, supprimé car la gestion d'API n'est plus utilisée.
-}
