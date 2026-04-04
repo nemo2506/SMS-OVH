bd5a6463a968994bba68533a2644a1c21a5de76f
MARC NAVARRO <navamarc@gmail.com>
2026-04-04 13:34:29 +0200

    chore: update app/src/main/java/com/miseservice/smsovh/domain/usecase/SendMessageUseCase.kt

---
+++ app/src/main/java/com/miseservice/smsovh/domain/usecase/SendMessageUseCase.kt
@@ -0,0 +1,23 @@
+package com.miseservice.smsovh.domain.usecase
+
+import com.miseservice.smsovh.domain.repository.SmsRepository
+import com.miseservice.smsovh.model.SendMessageRequest
+import com.miseservice.smsovh.model.SmsMessage
+import com.miseservice.smsovh.model.SendResult
+import javax.inject.Inject
+
+class SendMessageUseCase @Inject constructor(private val repository: SmsRepository) {
+    suspend operator fun invoke(request: SendMessageRequest): SendResult {
+        return if (request.base64Jpeg.isNullOrBlank()) {
+            repository.sendSms(
+                SmsMessage(
+                    from = request.senderId ?: "",
+                    to = request.destinataire,
+                    message = request.text
+                )
+            )
+        } else {
+            repository.sendMms(request)
+        }
+    }
+}
