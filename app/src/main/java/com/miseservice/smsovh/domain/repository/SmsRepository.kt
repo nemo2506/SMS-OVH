8049a74f893fe1a14c351b75cd286d68e464e409
MARC NAVARRO <navamarc@gmail.com>
2026-04-04 13:34:24 +0200

    chore: update app/src/main/java/com/miseservice/smsovh/domain/repository/SmsRepository.kt

--- app/src/main/java/com/miseservice/smsovh/domain/repository/SmsRepository.kt
+++ app/src/main/java/com/miseservice/smsovh/domain/repository/SmsRepository.kt
@@ -3,7 +3,10 @@ package com.miseservice.smsovh.domain.repository
 import com.miseservice.smsovh.model.SmsMessage
 import com.miseservice.smsovh.model.SendResult
 
+import com.miseservice.smsovh.model.SendMessageRequest
+
 interface SmsRepository {
     suspend fun sendSms(sms: SmsMessage): SendResult
+    suspend fun sendMms(request: SendMessageRequest): SendResult
 }
 
